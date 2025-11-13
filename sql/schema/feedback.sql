ae828bc2b57267997f6aee2056faa36467f1094f
jfbishop <juliet.f.bishop@gmail.com>
2025-11-13 12:37:42 -0500

    Update feedback.sql
    using timestamp instead

--- sql/schema/feedback.sql
+++ sql/schema/feedback.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS feedback (
     sample_id TEXT PRIMARY KEY,
     high_level_feedback TEXT,
     comments TEXT,  -- JSON stored as TEXT
-    created_ts DATETIME,
-    modified_ts DATETIME
+    created_ts TIMESTAMP,
+    modified_ts TIMESTAMP
 );
