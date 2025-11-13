04baaee0343cfda05b85abf669b305df58cc8edf
jfbishop <juliet.f.bishop@gmail.com>
2025-11-13 12:04:52 -0500

    Create judges.sql

---
+++ sql/schema/judges.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS judges (
+    sample_id TEXT PRIMARY KEY,
+    
+    -- Judge 1
+    judge_1_tone TEXT,
+    judge_1_detailed TEXT,
+    judge_1_grammar TEXT,
+    judge_1_structure TEXT,
+    judge_1_content TEXT,
+    
+    -- Judge 2
+    judge_2_tone TEXT,
+    judge_2_detailed TEXT,
+    judge_2_grammar TEXT,
+    judge_2_structure TEXT,
+    judge_2_content TEXT,
+    
+    -- Judge 3
+    judge_3_tone TEXT,
+    judge_3_detailed TEXT,
+    judge_3_grammar TEXT,
+    judge_3_structure TEXT,
+    judge_3_content TEXT,
+    
+    -- Timestamps
+    created_ts TEXT,
+    modified_ts TEXT
+);
