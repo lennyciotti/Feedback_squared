265730039ff96fb9af47eb55a382ee33d7a0c8ea
jfbishop <juliet.f.bishop@gmail.com>
2025-11-13 12:35:58 -0500

    Update samples.sql
    made essay_text longtext and ts datetime

--- sql/schema/samples.sql
+++ sql/schema/samples.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS samples (
     sample_id TEXT PRIMARY KEY,
-    essay_text TEXT,
+    essay_text LONGTEXT,
     grade_level TEXT,
     subject TEXT,
     prompt TEXT,
     grammar_level TEXT,
     flow_level TEXT,
     content_level TEXT,
-    created_ts TEXT,
-    modified_ts TEXT
+    created_ts DATETIME,
+    modified_ts DATETIME
 );
