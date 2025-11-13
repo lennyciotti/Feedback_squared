f3e9f92a426c514213531873498986579a4eb4ab
jfbishop <juliet.f.bishop@gmail.com>
2025-11-13 12:38:34 -0500

    Update samples.sql
    ok back to text for essay text and timestamp for ts

--- sql/schema/samples.sql
+++ sql/schema/samples.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS samples (
     sample_id TEXT PRIMARY KEY,
-    essay_text LONGTEXT,
+    essay_text TEXT,
     grade_level TEXT,
     subject TEXT,
     prompt TEXT,
     grammar_level TEXT,
     flow_level TEXT,
     content_level TEXT,
-    created_ts DATETIME,
-    modified_ts DATETIME
+    created_ts TIMESTAMP,
+    modified_ts TIMESTAMP
 );
