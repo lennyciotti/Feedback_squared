a0de19434e8bcd3711e0e9b79cdc9fc558679919
jfbishop <juliet.f.bishop@gmail.com>
2025-12-02 18:12:45 -0500

    Delete sql directory

--- sql/schema/feedback.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS feedback (
-    sample_id TEXT PRIMARY KEY,
-    high_level_feedback TEXT,
-    comments TEXT,  -- JSON stored as TEXT
-    created_ts TIMESTAMP,
-    modified_ts TIMESTAMP
-);
